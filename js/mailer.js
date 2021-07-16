0864ce6686c04851be682dcd255aad3a384e0454
EwarystDebian <ewaryst.lawecki@gmail.com>
2021-07-16 19:07:25 +0200

    visual changes and add new materials

---
+++ js/mailer.js
@@ -0,0 +1,25 @@
+const sendEmailButton = document.querySelector('.send-mail-button');
+const successAlert = document.querySelector('.success');
+const errorAlert = document.querySelector('.error');
+
+const sendMail = (e) => {
+     
+    const tempParams = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value,
+    }
+
+    if(tempParams.email && tempParams.message && tempParams.name && tempParams.subject){
+        e.preventDefault();
+        emailjs.send('service_hiaix0y', 'template_e04sja7', tempParams).then((res) => {
+            successAlert.style.display="block";
+        }).catch((error) => {
+            console.log(error)
+            errorAlert.style.display="block";
+        });
+    }
+}
+
+sendEmailButton.addEventListener('click', sendMail);
